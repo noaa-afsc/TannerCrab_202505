0506b45159190647276deca157a3988235018726
William Stockhausen <william.stockhausen@noaa.gov>
2025-03-25 11:31:34 -0700

    Added male Q likelihood profile setup.

---
+++ ModelRuns/ModelRuns-TCSAM02/LikelihoodProfiles_2024_22_03d5/LPs-Q-NMFS/tmp_win.bat
@@ -0,0 +1,5 @@
+echo on
+copy C:\Users\william.stockhausen\Work\StockAssessments-Crab\AssessmentModelDevelopment\VS_Code\tcsam02\_build\tcsam02.exe tcsam02.exe
+tcsam02  -rs -nox  -configFile ..\MCI.inp -phase 5  -calcOFL -nohess -pin tcsam02.pin
+
+
